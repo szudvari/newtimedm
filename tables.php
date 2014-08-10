10fc7a51f5629df9240be7bae5fc39ea8af485b6
Szabolcs Udvari <udvarisz@yahoo.com>
2014-08-10 11:04:13 +0200

    success

--- tables.php
+++ tables.php
@@ -173,3 +173,99 @@ EOT;
     }
     echo '</div>';
 }
+
+function getSuccesNewsletter($id) {
+    mysql_query("set names 'utf8'");
+    mysql_query("set character set 'utf8'");
+    $sql = "SELECT hirlevel.id, hirlevel.cim, hirlevel.datum, hirlevel.hirlevel_tipus, "
+            . "hirlevel.created_on, users.user "
+            . "from hirlevel "
+            . "left join users on hirlevel.created_by=users.id "
+            . "where hirlevel.id=$id;";
+    $result = mysql_query($sql);
+    $table = array();
+    while ($row = mysql_fetch_assoc($result)) {
+        $table[] = $row;
+    }
+    //Kicseréli a hírlvél típusokat string-re
+    for ($i = 0; ($i < mysql_num_rows($result)); $i++) {
+        switch ($table[$i]['hirlevel_tipus']) {
+            case 1:
+                $table[$i]['hirlevel_nev'] = "Travelo hírlevél";
+                break;
+            case 2:
+                $table[$i]['hirlevel_nev'] = "Life hírlevél";
+                break;
+            case 3:
+                $table[$i]['hirlevel_nev'] = "Life egyképes hírlevél";
+                break;
+        }
+    }
+    
+    echo <<<EOT
+    <div class="col-md-12">
+            <h3 class="page-header"><i class="fa fa-envelope"></i> Hírlevele elkészült</h3>
+    </div>
+    <div class="row news-ready-th">
+   <div class="col-md-1"> id </th>
+   <div class="col-md-2"> Cím </th>
+   <div class="col-md-1"> Kiküldés dátuma </th>
+   <div class="col-md-1"> Hírlevél típusa </th>
+   <div class="col-md-2"> Készítés ideje </th>
+   <div class="col-md-1"> Létrehozta </th>
+   <div class="col-md-1"> Megnéz </th>
+   <div class="col-md-1"> Szerkeszt </th>
+   <div class="col-md-1" tool-tip" title="HTML kód mentése"> HTML mentése </th>
+   <div class="col-md-1"> TXT mentése </th>
+    </div>
+   
+EOT;
+     $count = 0;
+    foreach ($table as $row) {
+        if ($count % 2 == 0)
+        {
+            echo '<div class="row news-ready-tr">';
+        }
+        else
+        {
+            echo '<div class="row news-ready-tr-sec">';
+        }
+        echo '<div class="col-md-1">' . $row['id'] . '</div>';
+        echo '<div class="col-md-2">' . $row['cim'] . '</div>';
+        echo '<div class="col-md-1">' . $row['datum'] . '</div>';
+        echo '<div class="col-md-1">' . $row['hirlevel_nev'] . '</div>';
+        echo '<div class="col-md-2">' . $row['created_on'] . '</div>';
+        echo '<div class="col-md-1">' . $row['user'] . '</div>';
+
+
+        switch ($row['hirlevel_tipus']) {
+            case 1:
+                $link = "travelo_nl_db.php";
+                break;
+            case 2:
+                $link = "life_nl_db.php";
+                break;
+            case 3:
+                $link = "life_op_db.php";
+                break;
+        }
+         echo '<div class="col-md-1"><a href="' . $link . '?hirlevel_id=' . $row['id'] . '" target="blank">Megnéz</a></div>';
+        echo '<div class="col-md-1"><a href="newsletter_edit.php?hirlevel_id=' . $row['id'] . '&hirlevel_type=' . $row['hirlevel_tipus'] . '" target="blank">Szerkeszt</a></div>';
+        echo '<div class="col-md-1 tool-tip" title="HTML kód mentése"><a href=' . $link . '?hirlevel_id=' . $row['id'] . '&save=1" target="blank">HTML kód mentése</a></div>';
+        switch ($row['hirlevel_tipus']) {
+            case 1:
+                $link = "travelo_nl_db_txt.php";
+                break;
+            case 2:
+                $link = "life_nl_db_txt.php";
+                break;
+            case 3:
+                $link = "life_op_db_txt.php";
+                break;
+        }
+        echo '<div class="col-md-1"><a href="' . $link . '?hirlevel_id=' . $row['id'] . '" target="blank">TXT változat mentése</a></div>';
+        echo '</div>';
+        $count++;
+    }
+    echo '</div>';
+}
