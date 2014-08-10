8e03505fbaf9e0f23acd702945327b5a600edcaa
Szabolcs Udvari <udvarisz@yahoo.com>
2014-08-10 11:07:35 +0200

    success 2

--- succes.php
+++ succes.php
@@ -1,8 +1,22 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ession_start();
+require_once 'functions.php';
+include_once 'config.php';
+include_once 'db.php';
+include_once 'html.php';
+include_once 'tables.php';
+htmlHead();
+navBar($_SESSION);
+if (!isset($_SESSION['login']))
+{
+    notLoggedIn();
+}
+else
+{
+    
+    $con = connectDb();
+    getSuccesNewsletter($id);
+    closeDb($con);
+}
+htmlEnd();
