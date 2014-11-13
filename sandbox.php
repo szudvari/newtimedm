3f910544a010ed67ea87679887972d1eb6548f95
Szabolcs Udvari <udvarisz@yahoo.com>
2014-11-13 13:47:54 +0100

    teszt

--- sandbox.php
+++ sandbox.php
@@ -4,6 +4,6 @@ include '/var/local/www/szallas.travelo.hu/public/inc/intravena_true.php';
 
 //print_r($int_true);
 sort ($int_true);
-foreach ($int_true as $site) {
-    echo "$site<br>";
+foreach ($int_true as $oldal) {
+    echo "$oldal<br>";
 }
